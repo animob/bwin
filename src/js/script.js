4609a03884e96692ab450f19f2f4b9b41c3973f0
animob <anngwork@gmail.com>
2024-07-01 00:57:22 +0300

    add: js open-hide script

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,26 @@ function populateCardNumbers() {
 }
 
 populateCardNumbers();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var openBtns = document.querySelectorAll('.bonus-card__open-btn');
+
+  openBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      var bonusCard = btn.closest('.bonus-card');
+
+      var infoBlock = bonusCard.querySelector('.bonus-card__info');
+
+      if (infoBlock.classList.contains('hidden')) {
+        infoBlock.classList.remove('hidden');
+        btn.textContent = 'Hide info';
+        btn.classList.add('active');
+      } else {
+        infoBlock.classList.add('hidden');
+        btn.textContent = 'More info';
+        btn.classList.remove('active');
+      }
+    });
+  });
+});
